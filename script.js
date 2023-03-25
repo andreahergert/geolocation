95e0614d69631fddfec6fe58c766e91eeb4dcbd0
Andrea Hergert <ahergert24@hotmail.com>
2023-03-25 11:44:08 -0700

    added what to do if speed is unavailable and added screenshot

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
 
 navigator.geolocation.watchPosition(data => {
-    speed.textContent = data.coords.speed;
+    if (data.coords.speed) {
+        speed.textContent = data.coords.speed;
+    } else {
+        speed.textContent = 'Speed not available';
+    }
     arrow.style.transform = `rotate(${data.coords.heading}deg)`;
 }, (err) => {
     console.error(err);
